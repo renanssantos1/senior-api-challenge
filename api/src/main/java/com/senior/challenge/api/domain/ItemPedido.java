88f2f14b19ad3385e6e361690e9554908c7a347e
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-30 09:04:24 -0300

    feat(domain): adicionado domain item pedido relacionado com pedido.

---
+++ api/src/main/java/com/senior/challenge/api/domain/ItemPedido.java
@@ -0,0 +1,31 @@
+package com.senior.challenge.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Table(name = "itens_pedido")
+@Entity(name = "ItemPedido")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "CHAR(36)")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(nullable = false)
+    private Double precoUnitario;
+}
