93427de24b1f2e649f68c07873cb54b5b6a67271
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-31 11:39:08 -0300

    fix(bean): ajuste da validacao do status e desconto.

--- api/src/main/java/com/senior/challenge/api/dto/DadosCadastroPedido.java
+++ api/src/main/java/com/senior/challenge/api/dto/DadosCadastroPedido.java
@@ -3,17 +3,16 @@ package com.senior.challenge.api.dto;
 import com.senior.challenge.api.domain.ItemPedido;
 import com.senior.challenge.api.domain.Status;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record DadosCadastroPedido(
 
-        @NotBlank
+        @NotNull
         Status status,
 
-        @NotBlank
+        @NotNull
         Double desconto,
 
         @NotNull
