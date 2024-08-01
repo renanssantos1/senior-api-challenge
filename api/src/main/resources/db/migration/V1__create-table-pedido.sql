9b2a2c0b6682b4ea22e05bee0eebe4734baf5473
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-01 20:44:46 -0300

    fix(database-packages): Ajuste query de pedido.

--- api/src/main/resources/db/migration/V1__create-table-pedido.sql
+++ api/src/main/resources/db/migration/V1__create-table-pedido.sql
@@ -1,5 +1,5 @@
-CREATE TABLE pedido (
+CREATE TABLE pedidos (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    status VARCHAR(50) NOT NULL DEFAULT 'Aberto' CHECK (status IN ('Aberto', 'Fechado')),
-    desconto NUMERIC(5, 2) DEFAULT 0 NOT NULL
+    status VARCHAR(50) NOT NULL,
+    desconto DOUBLE PRECISION NOT NULL
 );
