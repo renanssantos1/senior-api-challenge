c6fdd7eff5242ef84e759e7f6dc5f8511f6ceb98
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-08-02 21:16:42 -0300

    feat(item_pedidos): removido scripts sql antigos.

--- api/src/main/resources/db/migration/V2__create-table-produto-servico.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE produto_servico (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    preco NUMERIC(10, 2) NOT NULL,
-    ativo BOOLEAN DEFAULT TRUE NOT NULL,
-    is_produto BOOLEAN NOT NULL CHECK (is_produto IN (TRUE, FALSE))
-);
